2895a1e0ef433e0a269c0395300b86a0fca559b5
Kolli-Sai <116951565+Kolli-Sai@users.noreply.github.com>
2022-12-27 15:08:31 +0530

    counter added

--- js/counter.js
+++ js/counter.js
@@ -2,6 +2,13 @@ function updateCounter() {
     const counter = document.getElementById('counter');
     //increment the counter everytime user visits the page by storing in local storage
     let count = localStorage.getItem('visiterCount');
+    console.log(count)
+
+    if(count== String(null)){
+        count = 1;
+    }else{
+        count = parseInt(count) + 1
+    }
     /**
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
